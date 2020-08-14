71d587db7d6f73c0f01e5d87bc9a7b7e41d3b57e
Chelsea-exe <cjackson145@student.gsu.edu>
2020-08-14 02:02:31 -0400

    added files for landing page, added new html files, styling landing

--- tracker.js
+++ tracker.js
@@ -1,57 +1,3 @@
-// let btn = document.getElementById("submit-btn");
-// //set variable btn to the html button id
-        
-//         btn.addEventListener("click",()=>{
-//         let text = document.getElementById("input-text").value;
-//         console.log("button was pressed");
-//         //added a event once btn is pressed taking the value of what was typed in the form
-
-//             fetch('https://api.covid19api.com/summary')
-//             .then((covidData)=>{
-//                 return covidData.json();
-//             })
-//             .then((getData)=>{
-//                 console.log(getData);
-//                 console.log("api was contacted");
-//                 var content = document.querySelector(".api-data"); 
-
-//                 var box = content.lastElementChild;  
-//                 while (box) { 
-//                     content.removeChild(box); 
-//                     box = content.lastElementChild; 
-//                 } 
-
-//                 var countriesIndex = 0;
-//                 for(var i = 0; i < 185; i++){
-//                     if(getData.Countries[i].Country.toLowerCase() == text.toLowerCase()){
-//                         countriesIndex = i;
-//                         break;
-//                     }
-//                     else {
-//                         var hideData = document.querySelector(".api-data");
-//                         hideData.style.display = "none";
-//                         alert("No information for that country")
-//                         break;
-//                     }
-//                 }
-//                 let data = document.querySelector(".api-data");
-//                 data.innerHTML = `<div class="data-boxes">
-//                                 <div class="country-index">
-//                                     <span>Covid-19 Cases in ${getData.Countries[countriesIndex].Country}</span>
-//                                 </div>
-//                                 <div class="total-data">
-//                                     <div><p>Total Confirmed</p> ${getData.Countries[countriesIndex].TotalConfirmed}</div>
-//                                     <div><p>Total Deaths</p> ${getData.Countries[countriesIndex].TotalDeaths}</div>
-//                                     <div><p>Total Recovered</p> ${getData.Countries[countriesIndex].TotalRecovered}</div>
-//                                 </div>
-//                                 <div class="new-data">
-//                                     <div><p>New Confirmed</p> ${getData.Countries[countriesIndex].NewConfirmed}</div>
-//                                     <div><p>New Deaths</p> ${getData.Countries[countriesIndex].NewDeaths}</div>
-//                                     <div><p>New Recovered</p> ${getData.Countries[countriesIndex].NewRecovered}</div>
-//                                     </div>
-//                                 </div>`;
-//             })
-//         })
 let portfolio = document.getElementById("port-btn");
 portfolio.addEventListener("click", () => {
     console.log("portfolio button was pressed")
@@ -62,24 +8,33 @@ let btn = document.getElementById("submit-btn");
         
         btn.addEventListener("click",()=>{
         let text = document.getElementById("input-text").value;
+        /* text is set to the input-text from the HTML with the value of what the user
+        typed to be used in a function for search of the coutries within the json.
+         */
         console.log("button was pressed");
         //added a event once btn is pressed taking the value of what was typed in the form
 
             fetch('https://api.covid19api.com/summary')
             .then((covidData)=>{
                 return covidData.json();
+                /* have the server get the information from the api link above and
+                return with the json named covidData
+                */
             })
-            //
             .then((getData)=>{
                 console.log(getData);
                 console.log("api was contacted");
-                var content = document.querySelector(".api-data"); 
+                var content = document.querySelector(".api-data");
+                /*then take the getData that's within the json, have the getData be printed in
+                the console and set a variable called content to api-data div that's within the
+                HTML.
+                */
 
-                var box = content.lastElementChild;  
-                while (box) { 
-                    content.removeChild(box); 
-                    box = content.lastElementChild; 
-                } 
+                // var box = content.lastElementChild;  
+                // while (box) { 
+                //     content.removeChild(box); 
+                //     box = content.lastElementChild; 
+                // }
 
                 var countriesIndex = 0;
                 for(var i = 0; i < 185; i++){
@@ -89,12 +44,23 @@ let btn = document.getElementById("submit-btn");
                     }
                     
                 }
-                if(countriesIndex==0) {
+                /* countriesIndex to start at 0 then for loop created for the json of 186 countries
+                available inside the json. if statement, the user types the value in upper case or lower 
+                case the .toLowerCase the method will automatically put the value from text in lower case.
+                setting i to countriesIndex.
+                 */
+
+                if(countriesIndex == 0) {
                         var hideData = document.querySelector(".api-data");
                         hideData.style.display = "none";
                         alert("No information for that country at the moment! Sorry.")
                         // break;
                     }
+
+                /* another if statement setting the countriesIndex back to zero when the user's text value doesn't
+                match the countries that's inside the json the following alert message will appear and the style display
+                won't show, instead of the country Afghanistan information appearing.
+                 */
                 // else if(countriesIndex === '') {
                 //     alert("Please enter a country!")
                 // }
@@ -116,5 +82,7 @@ let btn = document.getElementById("submit-btn");
                                     </div>
                                 </div>`;
                                 }
+                /*the else statement once the value of the text matches the country in the json from the api, the data will appear in the api-data
+                div with the .innerHTML method with the following divs and the json ${getData.Countries}*/
             })
         })
