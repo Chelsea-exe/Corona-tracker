f9cee167b81ba2d69f12a6a2e9d98d8c4d669a79
Chelsea-exe <cjackson145@student.gsu.edu>
2020-08-15 00:55:21 -0400

    testing different google api maps to find users current location to search for covid-19 testing clinics

---
+++ testing.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    // alert($);
+    $('#find-btn').click(function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                console.log(position);
+                $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +"&sensor=false", function(data) {
+                    console.log(data);
+                })
+            })
+        }
+    })
+        // if('geolocation' in navigator) {
+        //     console.log('geolocation available');
+        //     navigator.geolocation.getCurrentPosition(function(position) {
+        //         console.log(position.coords);
+        //     });   
+        // }
+        // else {
+        //     console.log('geoloation is not available');
+        //     alert("geolocation is not available, use the search bar to locate covid-19 testing centers.")
+        // }
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(function(position) {
+    //         console.log(position);
+    //         console.log("location is located");
+    //         $.get("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.longitude + "&sensor=false", function(data) {
+    //             console.log(data);
+    //             console.log("here is your location!");
+    //         })
+    //     })
+    // } 
+});
+/* https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCh_Qf_xs-4uCVn8-tJm4xc0nnMMsqAzVo */
